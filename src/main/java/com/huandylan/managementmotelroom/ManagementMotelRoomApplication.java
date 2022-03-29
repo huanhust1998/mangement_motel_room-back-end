cfce06f872c776fc3bcdebc62875ea5cc74c2533
Xuan Huan Ninh <huannx@mintpotvn.com>
2022-03-29 10:13:55 +0700

    create project

---
+++ src/main/java/com/huandylan/managementmotelroom/ManagementMotelRoomApplication.java
@@ -0,0 +1,13 @@
+package com.huandylan.managementmotelroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementMotelRoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagementMotelRoomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/huandylan/managementmotelroom/ManagementMotelRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huandylan.managementmotelroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementMotelRoomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
